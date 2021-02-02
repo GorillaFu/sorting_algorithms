17816cc63e98d9b9c7f497d2ee24d795e7c42bb7
Lohkrii <joshualopez0429@gmail.com>
2021-02-02 20:20:57 +0000

    Fixed typo in edge case

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *node, *temp;
 
-	if (*list == NULL || (*list) == NULL)
+	if (list == NULL || (*list) == NULL)
 		return;
 	node = (*list)->next;
 	while (node)
