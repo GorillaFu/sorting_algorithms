35733c1ff011ab61c03e8f76b6f78246ec422677
Lohkrii <joshualopez0429@gmail.com>
2021-02-02 18:01:49 +0000

    Finished Main Insertion code. Working on bugs

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,21 +2,28 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t node = *list, temp;
+	listint_t *node, *temp_n, *traverse, *temp_p;
 
-	if (*list != NULL || (*list)->next != NULL)
+	while (*list != NULL || (*list)->next != NULL)
 	{
+		node = *list;
 		traverse = node;
+		if (traverse->next != NULL)
+			temp_n = node->next;
 		while (node->n < node->prev->n)
 		{
-			temp_n = node->next;
-			temp_node = node;
+			temp_p = node->prev;
 			node = node->prev;
-			node->next = temp_node;
-			temp_node->next = temp_n;
-			temp_n->prev = temp_node;
-			print_list(**list);
+			node->next = temp_p;
+			node->prev = temp_p->prev;
+			temp_p->prev = node;
+			if (traverse->next != NULL)
+				temp_p->next = temp_n;
+			else
+				temp_p->next = NULL;
+			print_list(*list);
 		}
-		node = traverse->next;
+		if (traverse->next != NULL)
+			node = traverse->next;
 	}
 }
