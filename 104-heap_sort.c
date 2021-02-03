f6246e75222c0b8cce62e4fd147ce4b3e9a0377a
GorillaFu <jared@beguelin.com>
2021-02-03 20:45:41 +0000

    quicksort algorithm with hoare paritions scheme

---
+++ 104-heap_sort.c
@@ -0,0 +1,28 @@
+include "sort.h"
+
+/**
+ * swap_elements - swap the values of two array pointers
+ * @a: pointer to array element to be swapped
+ * @b: pointer to array element to be swapped
+ *
+ * Return: void function
+ */
+void swap_elements(int *a, int *b)
+{
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+void sift_down(int *array, int min, int max, size_t size)
+{
+}
+
+void heapify(int *array, size_t size)
+{
+
+}
+
+void heap_sort(int *array, size_t size)
+{
+}
--- sort.h
+++ sort.h
@@ -25,5 +25,5 @@ void print_array(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void quick_sort_hoare(int *array, size_t size);
 #endif
